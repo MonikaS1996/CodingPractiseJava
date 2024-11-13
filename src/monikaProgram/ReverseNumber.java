b053de85ddefffd4f8a6583aeab66aa17510e147
MonikaS1996 <sanasmonika12@gmail.com>
2024-11-13 13:08:17 +0530

    reverse number

--- src/monikaProgram/ReverseNumber.java
+++ src/monikaProgram/ReverseNumber.java
@@ -1,10 +1,26 @@
 package monikaProgram;
 
+import java.util.Scanner;
+
 public class ReverseNumber {
 
 	public static void main(String[] args) {
-		System.out.println("monika sanas");
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Enter the number");
+		int num = sc.nextInt();
+		int rev=0;
+		int rem;
+		
+		
+		while(num!=0) {
+			rem = num%10;
+			rev = rev*10 + rem;
+			num = num/10;
+			
+		}
+		
+		System.out.println("reverse number is :"+rev );
 	}
 
 }
---
+++ src/monikaProgram/ReverseNumberUsingString.java
@@ -0,0 +1,27 @@
+package monikaProgram;
+
+import java.util.Scanner;
+
+public class ReverseNumberUsingString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number: ");
+		
+		int num = sc.nextInt();
+		
+		String numString = Integer.toString(num);
+		String ReverseString = "";
+		for(int i=0; i<=numString.length()-1; i++) {
+			
+			ReverseString = numString.charAt(i) +  ReverseString;
+			
+		}
+		
+		
+		int ReverseNumber = Integer.parseInt(ReverseString);
+		
+		System.out.println("Reverse number is : "+ReverseNumber);
+	}
+
+}
